ee2e1d2996dbef24cb36e22d57e0623eb30fad79
Ngoc Nicholas <ngocnicholas@outlook.com>
2023-02-14 18:19:40 -0800

    Update Comment.cs
    Updated json properties.  Added comments.

--- AirtableApiClient/Comment.cs
+++ AirtableApiClient/Comment.cs
@@ -14,15 +14,17 @@ namespace AirtableApiClient
 
         [JsonPropertyName("author")]
         [JsonInclude]
-        public Author Author { get; set; }
+        public Author Author { get; internal set; }
 
         [JsonPropertyName("mentioned")] // mentioned may contain one or more multiple. Each user is a key value pair of UserMentioned in the dictionary.
         [JsonInclude]
+        // NOTE: UserMentioned is a derived class of GroupMentioned. It inherits all properties from GrouplUser and has the Email property
+        // that GroupMentioned does not have.
         public Dictionary<string, UserMentioned> Mentioned { get; set; } = new Dictionary<string, UserMentioned>();
 
         [JsonPropertyName("text")]
         [JsonInclude]
-        public string Text { get; set; }
+        public string Text { get; internal set; }
 
         [JsonPropertyName("createdTime")]
         [JsonInclude]
@@ -32,6 +34,18 @@ namespace AirtableApiClient
         [JsonInclude]
         public DateTime? LastUpdatedTime { get; internal set; }
 
+
+        //----------------------------------------------------------------------------
+        //
+        // Comment.GetTextWithMentionedDisplayNames
+        //
+        // Replace the user ID in the text with the user's Display Name
+        // Ex: Comment Text is: "Al Held should work. @[usr1w2fdcVFbu7ug3]  good morning @[UgpBw4fdcVFbu5ug6] I thinks that's it."
+        // The output Text will be: "al Held should work. Ngoc Nicholas  good morning Foo Bars I thinks that's it."
+        // Each mentioned is a Dictionary. In the example above, The Dictionary contains two
+        // <Key, Value> pairs representing 2 UserMentioned objects.
+        //
+        //----------------------------------------------------------------------------
         public string GetTextWithMentionedDisplayNames(Dictionary<string, UserMentioned> mentioned)
         {
            string commentText = Text;
@@ -40,9 +54,10 @@ namespace AirtableApiClient
                 bool hasSingleUser = false;
                 foreach (KeyValuePair<string, UserMentioned> usrOrUgp in mentioned)
                 {
-                    if (!string.IsNullOrEmpty((string)usrOrUgp.Value.Email))
+                    if (!string.IsNullOrEmpty((string)usrOrUgp.Value.Email))        // Group user does not have Email.
                     {
                         hasSingleUser = true;
+                        break;
                     }
                 }
 
@@ -59,6 +74,14 @@ namespace AirtableApiClient
             }
             return commentText;
         }
+
+
+        //----------------------------------------------------------------------------
+        //
+        // Comment.ReplaceUserOrGroupIdWithDisplayName
+        //
+        //
+        //----------------------------------------------------------------------------
         private string ReplaceUserOrGroupIdWithDisplayName(Regex pattern, string text, Dictionary<string, UserMentioned> mentioned)
         {
             var matches = pattern.Matches(text);
@@ -66,14 +89,16 @@ namespace AirtableApiClient
             string value;
             int index;
 
+            // Loop from the end count because the commentText keeps changing.
             for (int count = matches.Count - 1; count >= 0; count--)
             {
-                value = matches[count].Value;
-                string key = value.Substring(2).TrimEnd(']');    // Get rid of "@[" at beginning and ']' at the end
+                value = matches[count].Value;                    // @[UgpBw4fdcVFbu5ug6]
+                string key = value.Substring(2).TrimEnd(']');    // Get rid of "@[" at beginning and ']' at the end.The resulting string is the key for the dictionary of Mentioned objects.
 
                 index = matches[count].Groups[0].Index;
                 if (mentioned.ContainsKey(key))
                 {
+                    // Remove matched value @[UgpBw4fdcVFbu5ug6] in comment and replace it with the Display Name
                     commentText = commentText.Remove(index, value.Length).Insert(index, mentioned[key].DisplayName);
                 }
                 else
@@ -95,35 +120,35 @@ namespace AirtableApiClient
 
         [JsonPropertyName("email")]
         [JsonInclude]
-        public string Email { get; set; }
+        public string Email { get; internal set; }
 
         [JsonPropertyName("name")]
         [JsonInclude]
-        public string Name { get; set; }
+        public string Name { get; internal set; }
     }
 
 
-    public class UserGroupMentioned     // does not have an email
+    public class GroupMentioned     // does not have an email
     {
         [JsonPropertyName("type")]
         [JsonInclude]
-        public string Type { get; set; }
+        public string Type { get; internal set; }
 
         [JsonPropertyName("id")]
         [JsonInclude]
-        public string Id { get; set; }
+        public string Id { get; internal set; }
 
         [JsonPropertyName("displayName")]
         [JsonInclude]
-        public string DisplayName { get; set; }
+        public string DisplayName { get; internal set; }
     }
 
 
-    public class UserMentioned : UserGroupMentioned
+    public class UserMentioned : GroupMentioned
     {
         [JsonPropertyName("email")]
         [JsonInclude]
-        public string Email { get; set; }
+        public string Email { get; internal set; }
     }
 
 
